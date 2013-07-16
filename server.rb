97de158730250691488613a4a72a1416d4828bdd
Myles Byrne <myles@myles.id.au>
2013-07-16 13:35:54 -0500

    Initial application implementation

---
+++ server.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'haml'
+
+require './timeslots'
+require './mock_timeslots'
+
+# TODO: visually highlight current timeslot
+
+get '/' do
+  headers 'X-Frame-Options' => "GOFORIT"
+  haml :day
+end
+
+# For hacking on the UI
+get '/mock' do
+  @mock = true
+  haml :day
+end
+
+helpers do
+
+  def todays_timeslots
+    @mock ? MockTimeslots.load : Timeslots.load
+  end
+
+  def current_day_of_week
+    Time.now.strftime("%A")
+  end
+
+end
